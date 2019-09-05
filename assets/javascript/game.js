7efe1a44ed614ce265d22bc861ecbb7348edea37
Cerek Moreno <cerekmoreno@gmail.com>
2019-09-04 22:26:17 -0700

    finished coding rest function and got values to display on html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,100 @@
-var wins = 0
 
-var losses = 0
+var winCount = 0; 
+$(".wScore").text("Wins: " + winCount);
+var lossCount = 0; 
+$(".lScore").text("Losses: " + lossCount);
+var userNumber = 0; 
+var gameNumber = Math.floor(Math.random() * 101) + 19; 
+var gem1 = Math.floor(Math.random() * 12) + 1; 
+var gem2 = Math.floor(Math.random() * 12) + 1; 
+var gem3 = Math.floor(Math.random() * 12) + 1; 
+var gem4 = Math.floor(Math.random() * 12) + 1; 
+$(".gNumber").text("Game Number: " + gameNumber);
+$(".uNumber").text("User Score: " + userNumber);
+console.log(gameNumber);
+console.log("diamond" + " " + gem1);
+console.log("ruby" + " " + gem2);
+console.log("emerald" + " " + gem3);
+console.log("amethyst" + " " + gem4);
 
-var gem1 = Math.floor(Math.random() * 11) + 1;
-    console.log("diamond" + " " + gem1)
 
-var gem2 = Math.floor(Math.random() * 11) + 1;
-    console.log("ruby" + " " + gem2)
+function resetGame() {
+userNumber = 0;
+gameNumber = Math.floor(Math.random() * 101) + 19; 
+gem1 = Math.floor(Math.random() * 12) + 1; 
+gem2 = Math.floor(Math.random() * 12) + 1; 
+gem3 = Math.floor(Math.random() * 12) + 1; 
+gem4 = Math.floor(Math.random() * 12) + 1; 
+$(".gNumber").text("Game Number: " + gameNumber);
+$(".uNumber").text("User Number: " + userNumber);
+console.log(gameNumber);
+console.log("diamond" + " " +gem1);
+console.log("ruby" + " " + gem2);
+console.log("emerald" + " " + gem3);
+console.log("amethyst" + " " +gem4);
+}
 
-var gem3 = Math.floor(Math.random() * 11) + 1;
-    console.log("emerald" + " " + gem3)
 
-var gem4 = Math.floor(Math.random() * 11) + 1;
-    console.log("amethyst" + " " +gem4)
+function winGame() {
+alert("You win! Lets go another round");
+winCount = winCount + 1;
+$(".wScore").text("Wins: " + winCount);
+resetGame();
+}
 
-var yourScore = 0
 
-var numberTarget = Math.floor(Math.random() * 111) + 19;
-    console.log("Number Target", + numberTarget);
+function loseGame() {
+alert("oh no try again :(");
+lossCount = lossCount + 1;
+$(".lScore").text("Losses: " + lossCount);
+resetGame();
+}
+
+
+$(".gem1").click(function() {
+userNumber = userNumber + gem1;
+console.log("User Number: " + userNumber);
+$(".userNumber").text("User Number: " + userNumber);
+  if (userNumber === gameNumber) {
+    winGame();
+  } else if (userNumber > gameNumber) {
+    loseGame();
+  }
+});
+
+
+$(".gem2").click(function() {
+    userNumber = userNumber + gem2;
+    console.log("User Number: " + userNumber);
+    $(".userNumber").text("User Number: " + userNumber);
+      if (userNumber === gameNumber) {
+        winGame();
+      } else if (userNumber > gameNumber) {
+        loseGame();
+      }
+    });
+
+
+$(".gem3").click(function() {
+    userNumber = userNumber + gem3;
+    console.log("User Number: " + userNumber);
+    $(".userNumber").text("User Number: " + userNumber);
+      if (userNumber === gameNumber) {
+        winGame();
+      } else if (userNumber > gameNumber) {
+        loseGame();
+      }
+    });
+
+
+    $(".gem4").click(function() {
+        userNumber = userNumber + gem4;
+        console.log("User Number: " + userNumber);
+        $(".userNumber").text("User Number: " + userNumber);
+          if (userNumber === gameNumber) {
+            winGame();
+          } else if (userNumber > gameNumber) {
+            loseGame();
+          }
+        });
 
