8c2a581c68178f89f6a550932895b446f99c9a22
Cerek Moreno <cerekmoreno@gmail.com>
2019-09-04 20:58:15 -0700

    added random number generator to variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+var wins = 0
+
+var losses = 0
+
+var gem1 = Math.floor(Math.random() * 11) + 1;
+    console.log("diamond" + " " + gem1)
+
+var gem2 = Math.floor(Math.random() * 11) + 1;
+    console.log("ruby" + " " + gem2)
+
+var gem3 = Math.floor(Math.random() * 11) + 1;
+    console.log("emerald" + " " + gem3)
+
+var gem4 = Math.floor(Math.random() * 11) + 1;
+    console.log("amethyst" + " " +gem4)
+
+var yourScore = 0
+
+var numberTarget = Math.floor(Math.random() * 111) + 19;
+    console.log("Number Target", + numberTarget);
+
